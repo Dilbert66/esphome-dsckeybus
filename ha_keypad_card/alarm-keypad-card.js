d7d06f81b3be8a9a395be46fcaf29af4e4e0f5c0
Dilbert66 <aturbide99@gmail.com>
2025-03-26 09:16:00 -0400

    - more cosmetic tweaks

--- ha_keypad_card/alarm-keypad-card.js
+++ ha_keypad_card/alarm-keypad-card.js
@@ -12,7 +12,7 @@ class AlarmKeypadCard extends LitElement {
  
   render() {
     return html`
-   <ha-card header="${this._title}">
+   <ha-card header="${this._title}" color-scheme="${this._scheme}">
         <div id="zoom" style="${this._scale}">
           <div class='flex-container' @click="${this.stopPropagation}">
               <div class='keypad'>
@@ -404,14 +404,14 @@ class AlarmKeypadCard extends LitElement {
       this._status_G_color=(config.status_G_color != null)?config.status_G_color:"green" ;  
       this._status_H_color=(config.status_H_color != null)?config.status_H_color:"green" ;  
 
-      this._lcdbgcolor=(config.lcd_bg_color != null)?config.lcd_bg_color:"var((--divider-color)"; 
+      this._lcdbgcolor=(config.lcd_bg_color != null)?config.lcd_bg_color:"var((--input-fill-color)"; 
       this._lcdtextcolor=(config.lcd_text_color != null)?config.lcd_text_color:"var(--primary-text-color)"; 
       this._buttonbgcolor=(config.button_bg_color != null)?config.button_bg_color:"var(--input-fill-color)";  
-      this._buttontextcolor=(config.button_text_color != null)?config.button_text_color:"var(--primary-text-color)"; 
-      this._sensorlabelcolor=(config.sensor_label_color != null)?config.sensor_label_color:"var(--primary-text-color)" ;  
-      this._buttonhovercolor=(config.sensor_hover_color != null)?config.sensor_hover_color:"var(--secondary-background-color)" ; 
+      this._buttontextcolor=(config.button_text_color != null)?config.button_text_color:"var(--primary-color)"; 
+      this._sensorlabelcolor=(config.sensor_label_color != null)?config.sensor_label_color:"var(--accent-color)" ;  
+      this._buttonhovercolor=(config.button_hover_color != null)?config.button_hover_color:"var(--outline-hover-color)" ; 
       this._buttonactivecolor=(config.button_active_color != null)?config.button_active_color:"var(--secondary-text-color)" ;  
-      this._buttonfocuscolor=(config.button_focus_color != null)?config.button_focus_color:"var(--secondary-background-color)" ;  
+      this._buttonfocuscolor=(config.button_focus_color != null)?config.button_focus_color:"var(--outline-color)" ;  
       this._sensoroffcolor=(config.sensor_off_color != null)?config.sensor_off_color:"var(--disabled-text-color)" ;   
   
      
@@ -447,6 +447,7 @@ class AlarmKeypadCard extends LitElement {
 
   set hass(hass) {
     this._hass = hass;
+    this._scheme=hass.themes.darkMode?"dark":"light";
     this._kpdline1 = this._hass.states[this._config.disp_line1];
     this._kpdline2 = this._hass.states[this._config.disp_line2];
     this._kpdbeep = this._hass.states[this._config.beep];
@@ -568,31 +569,58 @@ class AlarmKeypadCard extends LitElement {
 
 static get styles() {
     return css`
+
+
+
  
         @keyframes mdc-ripple-fg-radius-in{from{animation-timing-function:cubic-bezier(0.4, 0, 0.2, 1);transform:translate(var(--mdc-ripple-fg-translate-start, 0)) scale(1)}to{transform:translate(var(--mdc-ripple-fg-translate-end, 0)) scale(var(--mdc-ripple-fg-scale, 1))}}
         @keyframes mdc-ripple-fg-opacity-in{from{animation-timing-function:linear;opacity:0}to{opacity:var(--mdc-ripple-fg-opacity, 0)}}
         @keyframes mdc-ripple-fg-opacity-out{from{animation-timing-function:linear;opacity:var(--mdc-ripple-fg-opacity, 0)}to{opacity:0}}.mdc-ripple-surface--test-edge-var-bug{--mdc-ripple-surface-test-edge-var: 1px solid #000;visibility:hidden}.mdc-ripple-surface--test-edge-var-bug::before{border:var(--mdc-ripple-surface-test-edge-var)}.mdc-button{font-family:Roboto,sans-serif;-moz-osx-font-smoothing:grayscale;-webkit-font-smoothing:antialiased;font-size:.875rem;line-height:2.25rem;font-weight:500;letter-spacing:.0892857143em;text-decoration:none;text-transform:uppercase;--mdc-ripple-fg-size: 0;--mdc-ripple-left: 0;--mdc-ripple-top: 0;--mdc-ripple-fg-scale: 1;--mdc-ripple-fg-translate-end: 0;--mdc-ripple-fg-translate-start: 0;-webkit-tap-highlight-color:rgba(0,0,0,0);will-change:transform,opacity;padding:0 8px 0 8px;display:inline-flex;position:relative;align-items:center;justify-content:center;box-sizing:border-box;min-width:64px;height:36px;border:none;outline:none;line-height:inherit;user-select:none;-webkit-appearance:none;overflow:hidden;vertical-align:middle;border-radius:4px}.mdc-button::before,.mdc-button::after{position:absolute;border-radius:50%;opacity:0;pointer-events:none;content:""}.mdc-button::before{transition:opacity 15ms linear,background-color 15ms linear;z-index:1}.mdc-button.mdc-ripple-upgraded::before{transform:scale(var(--mdc-ripple-fg-scale, 1))}.mdc-button.mdc-ripple-upgraded::after{top:0;left:0;transform:scale(0);transform-origin:center center}.mdc-button.mdc-ripple-upgraded--unbounded::after{top:var(--mdc-ripple-top, 0);left:var(--mdc-ripple-left, 0)}.mdc-button.mdc-ripple-upgraded--foreground-activation::after{animation:225ms mdc-ripple-fg-radius-in forwards,75ms mdc-ripple-fg-opacity-in forwards}.mdc-button.mdc-ripple-upgraded--foreground-deactivation::after{animation:150ms mdc-ripple-fg-opacity-out;transform:translate(var(--mdc-ripple-fg-translate-end, 0)) scale(var(--mdc-ripple-fg-scale, 1))}.mdc-button::before,.mdc-button::after{top:calc(50% - 100%);left:calc(50% - 100%);width:200%;height:200%}.mdc-button.mdc-ripple-upgraded::after{width:var(--mdc-ripple-fg-size, 100%);height:var(--mdc-ripple-fg-size, 100%)}.mdc-button::-moz-focus-inner{padding:0;border:0}.mdc-button:active{outline:none}.mdc-button:hover{cursor:pointer}.mdc-button:disabled{background-color:transparent;color:rgba(0,0,0,.38);cursor:default;pointer-events:none}.mdc-button.mdc-button--dense{border-radius:4px}.mdc-button:not(:disabled){background-color:transparent}.mdc-button:not(:disabled){color:rgba(0,0,0,.38);color:var(--mdc-theme-primary, rgba(0,0,0,.38))}.mdc-button::before,.mdc-button::after{background-color:rgba(0,0,0,.38)}@supports not (-ms-ime-align: auto){.mdc-button::before,.mdc-button::after{background-color:var(--mdc-ripple-color, #03A9F4)}}.mdc-button:hover::before{opacity:.04}.mdc-button:not(.mdc-ripple-upgraded):focus(disabled)::before,.mdc-button.mdc-ripple-upgraded--background-focused::before{transition-duration:75ms;opacity:.12}.mdc-button:not(.mdc-ripple-upgraded)::after{transition:opacity 150ms linear}.mdc-button:not(.mdc-ripple-upgraded):active(disabled)::after{transition-duration:75ms;opacity:.64}.mdc-button.mdc-ripple-upgraded{--mdc-ripple-fg-opacity: 0.16}.mdc-button .mdc-button__icon{margin-left:0;margin-right:8px;display:inline-block;width:18px;height:18px;font-size:18px;vertical-align:top}[dir=rtl] .mdc-button .mdc-button__icon,.mdc-button .mdc-button__icon[dir=rtl]{margin-left:8px;margin-right:0}.mdc-button svg.mdc-button__icon{fill:rgba(0,0,0,.38)}.mdc-button--raised .mdc-button__icon,.mdc-button--unelevated .mdc-button__icon,.mdc-button--outlined .mdc-button__icon{margin-left:-4px;margin-right:8px}[dir=rtl] .mdc-button--raised .mdc-button__icon,.mdc-button--raised .mdc-button__icon[dir=rtl],[dir=rtl] .mdc-button--unelevated .mdc-button__icon,.mdc-button--unelevated .mdc-button__icon[dir=rtl],[dir=rtl] .mdc-button--outlined .mdc-button__icon,.mdc-button--outlined .mdc-button__icon[dir=rtl]{margin-left:8px;margin-right:-4px}.mdc-button--raised,.mdc-button--unelevated{padding:0 16px 0 16px}.mdc-button--raised:disabled,.mdc-button--unelevated:disabled{background-color:rgba(0,0,0,.12);color:rgba(0,0,0,.37)}.mdc-button--raised:not(:disabled),.mdc-button--unelevated:not(:disabled){background-color:rgba(0,0,0,.38)}@supports not (-ms-ime-align: auto){.mdc-button--raised:not(:disabled),.mdc-button--unelevated:not(:disabled){background-color:var(--mdc-theme-primary, rgba(0,0,0,.38))}}.mdc-button--raised:not(:disabled),.mdc-button--unelevated:not(:disabled){color:#fff;color:var(--mdc-theme-on-primary, #fff)}.mdc-button--raised::before,.mdc-button--raised::after,.mdc-button--unelevated::before,.mdc-button--unelevated::after{background-color:#fff}@supports not (-ms-ime-align: auto){.mdc-button--raised::before,.mdc-button--raised::after,.mdc-button--unelevated::before,.mdc-button--unelevated::after{background-color:var(--mdc-theme-on-primary, #fff)}}.mdc-button--raised:hover::before,.mdc-button--unelevated:hover::before{opacity:.08}.mdc-button--raised:not(.mdc-ripple-upgraded):focus::before,.mdc-button--raised.mdc-ripple-upgraded--background-focused::before,.mdc-button--unelevated:not(.mdc-ripple-upgraded):focus::before,.mdc-button--unelevated.mdc-ripple-upgraded--background-focused::before{transition-duration:75ms;opacity:.24}.mdc-button--raised:not(.mdc-ripple-upgraded)::after,.mdc-button--unelevated:not(.mdc-ripple-upgraded)::after{transition:opacity 150ms linear}.mdc-button--raised:not(.mdc-ripple-upgraded):active::after,.mdc-button--unelevated:not(.mdc-ripple-upgraded):active::after{transition-duration:75ms;opacity:.32}.mdc-button--raised.mdc-ripple-upgraded,.mdc-button--unelevated.mdc-ripple-upgraded{--mdc-ripple-fg-opacity: 0.32}.mdc-button--raised{box-shadow:0px 3px 1px -2px rgba(0, 0, 0, 0.2),0px 2px 2px 0px rgba(0, 0, 0, 0.14),0px 1px 5px 0px rgba(0,0,0,.12);transition:box-shadow 280ms cubic-bezier(0.4, 0, 0.2, 1)}.mdc-button--raised:hover,.mdc-button--raised:focus{box-shadow:0px 2px 4px -1px rgba(0, 0, 0, 0.2),0px 4px 5px 0px rgba(0, 0, 0, 0.14),0px 1px 10px 0px rgba(0,0,0,.12)}.mdc-button--raised:activebox-shadow:0px 5px 5px -3px rgba(0, 0, 0, 0.2),0px 8px 10px 1px rgba(0, 0, 0, 0.14),0px 3px 14px 2px rgba(0,0,0,.12)}.mdc-button--raised:disabled{box-shadow:0px 0px 0px 0px rgba(0, 0, 0, 0.2),0px 0px 0px 0px rgba(0, 0, 0, 0.14),0px 0px 0px 0px rgba(0,0,0,.12)}.mdc-button--outlined{border-style:solid;padding:8px 8px 8px 8px;border-width:1px}.mdc-button--outlined:disabled{border-color:rgba(0,0,0,.37)}.mdc-button--outlined:not(:disabled){border-color:rgba(0,0,0,.38);border-color:var(--mdc-button-outline-color, rgba(0,0,0,.38))}.mdc-button--dense{height:32px;font-size:.8125rem}.material-icons{font-family:var(--mdc-icon-font, "Material Icons");font-weight:normal;font-style:normal;font-size:var(--mdc-icon-size, 24px);line-height:1;letter-spacing:normal;text-transform:none;display:inline-block;white-space:nowrap;word-wrap:normal;direction:ltr;-webkit-font-feature-settings:"liga";-webkit-font-smoothing:antialiased}:host{display:inline-flex;outline:none}.mdc-button{flex:1}
+/*
+ha-card[color-scheme="dark"] {
+--unavailable: #303030;
+--btngrpbgcolor:  var(--card-background-color);
+--maingrpbgcolor: var(--card-background-color);
+--bgkeycolor: var(--card-background-color);
+--cmdkeycolor: var(--card-background-color);
+--textcolor: var(--switch-checked-color);
+--bordercolor: #404040;
+--bglcd: #748C74;
+--lcdtextcolor: #000;
+--hovercolor: #303030;
+}
 
-
+ha-card[color-scheme="light"] {
+--unavailable: #ccc;
+--btngrpbgcolor:  var(--primary-background-color);
+--maingrpbgcolor: var(--secondary-background-color);
+--bgkeycolor: var(--secondary-background-color);
+--cmdkeycolor: var(--secondary-background-color);
+--textcolor: var(--primary-text-color);
+--bglcd: #748C74;
+--lcdtextcolor: #000;
+--bordercolor: #898e94;
+--hovercolor: #c0c0c0;
+}
+*/
       :host {
            width: 100%;
            --lcdbg: var(--input-fill-color);
            --lcdtext: var(--primary-text-color);
-           --buttonbg: var(--input-disabled-fill-color);
-           --buttontext: var(--primary-text-color);
-           --sensorlabel: var(--primary-text-color);
+           --buttonbg: var(--input-fill-color);
+           --buttontext: var(--primary--color);
+           --sensorlabel: var(--accent-color);
            --sensoroff: var(--disabled-text-color);
-           --buttonhover: var(--secondary-background-color);
-           --buttonactive: var(secondary-text-color);
-           --buttonfocus: var(--secondary-background-color);
+           --buttonhover: var(--outline-hover-color);
+           --buttonactive: var(--secondary-text-color);
+           --buttonfocus: var(--outline-color);
         
        }
        ha-card {
           width: 100%;
-          padding-bottom: 16px;
-          padding-top: 16px;
           position: relative;
           font-size: calc(var(--base-unit));
+          padding-bottom: 16px;
         }
 
         .flex-container {
@@ -601,21 +629,18 @@ static get styles() {
             align-items: center;
         }
         
-        .separarator {
-            padding: 8px;
-        }
+
         .keypad_display {
           background-color: var(--lcdbg);
           border-radius: 10px;
-          width: 260px;
-          height: 60px;
+          width: 270px;
+          min-height: 40px;
           margin: auto;
-          margin-top: 10px;
-          margin-bottom: 10px;
+          margin-bottom: 3px;
           border: 1px solid var(--disabled-color);
           overflow: auto;
-          padding: 5px;
-          padding-top: 10px;
+          padding: 8px;
+         
         }
         .display_line {
           font-size: calc(var(--base-unit) * .8);
@@ -624,6 +649,7 @@ static get styles() {
           font-family: monospace;
           display: flex;
           justify-content: center;
+          font-family: "Courier New", monospace;
         }
         #display_line1 {
           padding-bottom: 10px;
